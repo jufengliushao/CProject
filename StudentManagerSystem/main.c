5d50a8755847e478fb7c7f432e9016c52bedad78
刘少锋 <jufengliushao@163.com>
2018-03-26 09:43:58 +0800

    s

---
+++ StudentManagerSystem/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StudentManagerSystem
+//
+//  Created by fumed on 2018/3/26.
+//  Copyright © 2018年 fumed. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
